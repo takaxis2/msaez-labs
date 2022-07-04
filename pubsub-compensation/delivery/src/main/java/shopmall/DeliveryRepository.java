8ff87913a95d4df4f7127cfdd81db574afe49f20
sjcu-cloud <108639319+sjcu-cloud@users.noreply.github.com>
2022-07-04 10:22:13 +0900

    Delete pubsub-compensation directory

--- pubsub-compensation/delivery/src/main/java/shopmall/DeliveryRepository.java
+++
@@ -1,8 +0,0 @@
-package shopmall;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
-
-
-}
