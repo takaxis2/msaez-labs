28cc434bea6f1e0b1004dcceee71f6f78893f3ce
sjcu-cloud <108639319+sjcu-cloud@users.noreply.github.com>
2022-07-04 10:09:07 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -12,4 +12,3 @@ sudo ./aws/install
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-
