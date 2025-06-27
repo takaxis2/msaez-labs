30b9fbf8f8b15131973874aabd081803ee60bc01
박용주 <apexacme@gmail.com>
2025-06-27 15:31:06 +0900

    Update init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -15,12 +15,9 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
 
 # awscli 설치
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -o awscliv2.zip
-sudo ./aws/install --update
-rm -rf aws awscliv2.zip
-
-
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "../awscliv2.zip"
+unzip -o ../awscliv2.zip
+sudo ./aws/install 
 
 # eksctl 설치
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
